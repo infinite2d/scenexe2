c9d6f56c233364e48b9a0e1f204c8fce52373b9b
infinite2d <143294424+infinite2d@users.noreply.github.com>
2024-04-27 13:51:23 -0400

    Create tankDataPaths.js

---
+++ tankDataPaths.js
@@ -0,0 +1,376 @@
+"weaponUpgradeMap": {
+	"node": {
+		"level": 1,
+		"upgrades": ["commander", "mono", "trapper"]
+	},
+	"commander": {
+		"level": 15,
+		"upgrades": ["alloy", "director", "fusion", "overseer"]
+	},
+	"mono": {
+		"level": 15,
+		"upgrades": ["alloy", "duo", "flank", "guard", "single", "sniper", "split"]
+	},
+	"trapper": {
+		"level": 15,
+		"upgrades": ["blockade", "fusion", "gamma", "guard", "rubble"]
+	},
+	"alloy": {
+		"level": 30,
+		"upgrades": ["compound", "conglomerate"]
+	},
+	"blockade": {
+		"level": 30,
+		"upgrades": ["barricade", "conglomerate", "stockade"]
+	},
+	"director": {
+		"level": 30,
+		"upgrades": ["compound", "factory", "manager"]
+	},
+	"duo": {
+		"level": 30,
+		"upgrades": ["arc", "gunner", "quad", "trio"]
+	},
+	"flank": {
+		"level": 30,
+		"upgrades": ["conglomerate", "overlord", "quad", "wake"]
+	},
+	"fusion": {
+		"level": 30,
+		"upgrades": ["conglomerate"]
+	},
+	"gamma": {
+		"level": 30,
+		"upgrades": ["beta", "engineer"]
+	},
+	"guard": {
+		"level": 30,
+		"upgrades": ["conglomerate", "wake"]
+	},
+	"overseer": {
+		"level": 30,
+		"upgrades": ["factory", "overlord"]
+	},
+	"rubble": {
+		"level": 30,
+		"upgrades": ["scrap"]
+	},
+	"single": {
+		"level": 30,
+		"upgrades": ["assassin", "compound", "destroyer"]
+	},
+	"sniper": {
+		"level": 30,
+		"upgrades": ["assassin", "destroyer", "wake"]
+	},
+	"split": {
+		"level": 30,
+		"upgrades": ["arc", "gunner", "quad", "spread", "trio"]
+	},
+	"arc": {
+		"level": 45,
+		"upgrades": ["disperse", "horizon", "octo", "penta"]
+	},
+	"assassin": {
+		"level": 45,
+		"upgrades": ["marksman", "streamliner"]
+	},
+	"barricade": {
+		"level": 45,
+		"upgrades": ["arsenal", "riot", "wave"]
+	},
+	"beta": {
+		"level": 45,
+		"upgrades": ["alpha", "architect", "flamethrower", "raider"]
+	},
+	"compound": {
+		"level": 45,
+		"upgrades": ["hybrid"]
+	},
+	"conglomerate": {
+		"level": 45,
+		"upgrades": ["amalgam"]
+	},
+	"destroyer": {
+		"level": 45,
+		"upgrades": ["annihilator", "hybrid", "manufacturer"]
+	},
+	"engineer": {
+		"level": 45,
+		"upgrades": ["architect", "arsenal", "flamethrower", "mechanic", "raider"]
+	},
+	"factory": {
+		"level": 45,
+		"upgrades": ["hatcher", "industry", "manufacturer"]
+	},
+	"gunner": {
+		"level": 45,
+		"upgrades": ["horizon", "minigun", "quadro", "streamliner"]
+	},
+	"manager": {
+		"level": 45,
+		"upgrades": ["executive", "hybrid", "manufacturer"]
+	},
+	"overlord": {
+		"level": 45,
+		"upgrades": ["emperor", "hatcher"]
+	},
+	"quad": {
+		"level": 45,
+		"upgrades": ["horizon", "octo"]
+	},
+	"scrap": {
+		"level": 45,
+		"upgrades": ["shrapnel"]
+	},
+	"spread": {
+		"level": 45,
+		"upgrades": ["disperse", "horizon", "penta"]
+	},
+	"stockade": {
+		"level": 45,
+		"upgrades": ["mechanic", "palisade"]
+	},
+	"trio": {
+		"level": 45,
+		"upgrades": ["industry", "penta", "quadro", "streamliner"]
+	},
+	"wake": {
+		"level": 45,
+		"upgrades": ["wave"]
+	},
+	"nova": {
+		"level": 75,
+		"upgrades": ["debris", "pulsar", "satellite"]
+	},
+	"debris": {
+		"level": 90,
+		"upgrades": ["asteroid", "charon"]
+	},
+	"pulsar": {
+		"level": 90,
+		"upgrades": ["blazar", "quasar"]
+	},
+	"satellite": {
+		"level": 90,
+		"upgrades": ["moon", "synope"]
+	},
+	"asteroid": {
+		"level": 135,
+		"upgrades": ["ceres", "placeholderD"]
+	},
+	"blazar": {
+		"level": 135,
+		"upgrades": ["placeholderA", "placeholderB", "star"]
+	},
+	"charon": {
+		"level": 135,
+		"upgrades": ["ceres", "placeholderE"]
+	},
+	"moon": {
+		"level": 135,
+		"upgrades": ["lunarus", "placeholderC"]
+	},
+	"novus": {
+		"level": 135,
+		"upgrades": ["hades", "minos", "thanatos"]
+	},
+	"quasar": {
+		"level": 135,
+		"upgrades": ["blitzar", "placeholderA", "star"]
+	},
+	"synope": {
+		"level": 135,
+		"upgrades": ["cluster", "lunarus"]
+	},
+	"hades": {
+		"level": 135,
+		"upgrades": ["sisyphos", "tartarus"]
+	},
+	"minos": {
+		"level": 135,
+		"upgrades": ["angelos", "zagreus"]
+	},
+	"thanatos": {
+		"level": 135,
+		"upgrades": ["ixion", "tantalus"]
+	},
+	"peacekeeper1": {
+		"level": 105,
+		"upgrades": ["peacekeeper2"]
+	},
+	"peacekeeper2": {
+		"level": 120,
+		"upgrades": ["peacekeeper3"]
+	},
+	"peacekeeper3": {
+		"level": 135,
+		"upgrades": ["peacekeeper4"]
+	},
+	"booster": {
+		"level": 1,
+		"upgrades": ["undercovercop"]
+	},
+	"generator3": {
+		"level": 1,
+		"upgrades": ["generator4"]
+	},
+	"generator4": {
+		"level": 1,
+		"upgrades": ["generator5"]
+	},
+	"generator5": {
+		"level": 1,
+		"upgrades": ["generator6"]
+	},
+	"generator6": {
+		"level": 1,
+		"upgrades": ["generator7"]
+	}
+},
+"bodyUpgradeMap": {
+	"abyssal": {
+		"level": 120,
+		"upgrades": ["erebus", "hecate", "hypnos", "nyx", "persephone"]
+	},
+	"armory": {
+		"level": 30,
+		"upgrades": ["brigade", "castle"]
+	},
+	"artillery": {
+		"level": 45,
+		"upgrades": ["bastion", "bombard"]
+	},
+	"base": {
+		"level": 1,
+		"upgrades": ["hangar", "hearth", "sentry", "smasher", "wall"]
+	},
+	"battleship": {
+		"level": 45,
+		"upgrades": ["mothership"]
+	},
+	"bonfire": {
+		"level": 30,
+		"upgrades": ["flare", "forge"]
+	},
+	"brigade": {
+		"level": 45,
+		"upgrades": ["bastion", "battalion"]
+	},
+	"cannon": {
+		"level": 30,
+		"upgrades": ["artillery", "brigade", "castle"]
+	},
+	"castle": {
+		"level": 45,
+		"upgrades": ["bastion", "bombard", "saw", "ziggurat"]
+	},
+	"celestial": {
+		"level": 75,
+		"upgrades": ["chasm", "corvus", "exosphere", "nebula", "triton"]
+	},
+	"chasm": {
+		"level": 90,
+		"upgrades": ["comet", "meteor", "void"]
+	},
+	"citadel": {
+		"level": 30,
+		"upgrades": ["castle"]
+	},
+	"corvus": {
+		"level": 90,
+		"upgrades": ["cygnus", "naos"]
+	},
+	"exosphere": {
+		"level": 90,
+		"upgrades": ["heliosphere", "meteor"]
+	},
+	"flare": {
+		"level": 45,
+		"upgrades": ["foundry", "inferno"]
+	},
+	"forge": {
+		"level": 45,
+		"upgrades": ["battalion", "foundry", "fountain"]
+	},
+	"fortress": {
+		"level": 30,
+		"upgrades": ["castle", "palace"]
+	},
+	"hangar": {
+		"level": 15,
+		"upgrades": ["warship"]
+	},
+	"hearth": {
+		"level": 15,
+		"upgrades": ["bonfire", "mender"]
+	},
+	"mender": {
+		"level": 30,
+		"upgrades": ["remedy"]
+	},
+	"nebula": {
+		"level": 90,
+		"upgrades": ["galaxy", "naos", "oberon", "pollux"]
+	},
+	"palace": {
+		"level": 45,
+		"upgrades": ["bastion", "ziggurat"]
+	},
+	"peacekeeper1": {
+		"level": 105,
+		"upgrades": ["peacekeeper2"]
+	},
+	"peacekeeper2": {
+		"level": 120,
+		"upgrades": ["peacekeeper3"]
+	},
+	"peacekeeper3": {
+		"level": 135,
+		"upgrades": ["peacekeeper4"]
+	},
+	"remedy": {
+		"level": 45,
+		"upgrades": ["fabricator", "fountain"]
+	},
+	"sentry": {
+		"level": 15,
+		"upgrades": ["armory", "cannon", "citadel", "turret"]
+	},
+	"smasher": {
+		"level": 15,
+		"upgrades": ["armory", "fortress", "spike"]
+	},
+	"spike": {
+		"level": 30,
+		"upgrades": ["brigade", "castle", "forge", "thorn"]
+	},
+	"stronghold": {
+		"level": 30,
+		"upgrades": ["castle", "palace"]
+	},
+	"thorn": {
+		"level": 45,
+		"upgrades": ["bastion", "battalion", "foundry", "fountain", "saw"]
+	},
+	"triplet": {
+		"level": 45,
+		"upgrades": ["battalion", "quadruplet"]
+	},
+	"triton": {
+		"level": 90,
+		"upgrades": ["hyperion", "oberon"]
+	},
+	"turret": {
+		"level": 30,
+		"upgrades": ["artillery", "brigade", "castle", "triplet"]
+	},
+	"wall": {
+		"level": 15,
+		"upgrades": ["citadel", "fortress", "stronghold"]
+	},
+	"warship": {
+		"level": 30,
+		"upgrades": ["battleship"]
+	}
+}
